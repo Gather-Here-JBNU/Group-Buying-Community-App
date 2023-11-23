d5337a0a69d6f937108598ac2d14c2fa0292a1c7
DodoJBNU <dh723@jbnu.ac.kr>
2023-11-23 23:35:29 +0900

    Move : MainActivity를 LoginLogoutSelectActivity로 이동.

--- app/src/main/java/com/example/cloudcomputingproject/MainActivity.java
+++ app/src/main/java/com/example/cloudcomputingproject/MainActivity.java
@@ -2,32 +2,13 @@ package com.example.cloudcomputingproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button login_btn, register_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        login_btn = findViewById(R.id.login_btn);
-        register_btn = findViewById(R.id.register_btn);
-
-        login_btn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(getApplicationContext(), LoginActivty.class);
-                startActivity(intent);
-            }
-        });
-        register_btn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
