76eeae830018028b48fa9469a46b4bdd7086f2da
B-JoCharm <121530622+B-JoCharm@users.noreply.github.com>
2023-11-26 02:56:41 +0900

    CategoryAdapter.java - category.xml의 리사이클러뷰에 객체들을 생성해줄 코드 작성 CategoryActivity.java - category_item_template.xml의 템플릿을 사용하여 만든 객체들을 배치하도록 코드 구현 CategoryItem.java - category_item_template.xml의 템플릿을 사용하여 객체를 만들수 있도록 클래스 구현
    category_item_template.xml
    - 찜버튼 클릭시 이미지 변경 구현
    
    dimens.xml
    - 크기와 관련된 리소스들을 관리할 파일 생성
    
    MainActivity.java
    - 실행 시 바로 CategoryActivity.java가 실행되도록 변경 (테스트용으로 작성)
    
    추가적으로 변경된 사항들은 디자인적인 소소한 변경들.

---
+++ app/src/main/java/com/example/cloudcomputingproject/CategoryActivity.java
@@ -0,0 +1,58 @@
+package com.example.cloudcomputingproject;
+
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.cloudcomputingproject.categorypage.CategoryAdapter;
+import com.example.cloudcomputingproject.categorypage.CategoryItem;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private CategoryAdapter adapter;
+    private List<CategoryItem> categoryList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.category);
+
+        recyclerView = findViewById(R.id.category_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        categoryList = new ArrayList<>();
+        categoryList.add(new CategoryItem("음식배달"));
+        categoryList.add(new CategoryItem("도서"));
+        categoryList.add(new CategoryItem("운동기구"));
+
+
+        adapter = new CategoryAdapter(categoryList);
+        recyclerView.setAdapter(adapter);
+
+        // CustomItemDecoration을 추가합니다.
+        int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.item_decoration_margin);
+        recyclerView.addItemDecoration(new CustomItemDecoration(spacingInPixels));
+    }
+
+    // CustomItemDecoration 클래스를 내부 클래스로 정의합니다.
+    class CustomItemDecoration extends RecyclerView.ItemDecoration {
+        private final int verticalSpaceHeight;
+
+        public CustomItemDecoration(int verticalSpaceHeight) {
+            this.verticalSpaceHeight = verticalSpaceHeight;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            outRect.bottom = verticalSpaceHeight;
+            // 첫 번째 아이템에는 상단에도 공간을 추가합니다.
+            if (parent.getChildAdapterPosition(view) == 0) {
+                outRect.top = verticalSpaceHeight;
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/cloudcomputingproject/MainActivity.java
+++ app/src/main/java/com/example/cloudcomputingproject/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.cloudcomputingproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        setContentView(R.layout.post);
+        //setContentView(R.layout.post_preview_template);
+        // CategoryActivity를 시작하는 Intent를 생성합니다.
+        Intent intent = new Intent(this, CategoryActivity.class);
+        startActivity(intent);
+
+        // MainActivity를 종료하여, 뒤로 가기 버튼을 눌렀을 때 MainActivity로 돌아가지 않게 합니다.
+        finish();
     }
 }
