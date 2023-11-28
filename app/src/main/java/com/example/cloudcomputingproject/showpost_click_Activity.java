e34cd593ed417a939db21d70e18089c85b0ec7af
kimjehyeon <dlsrks0631@naver.com>
2023-11-29 01:24:19 +0900

    update

---
+++ app/src/main/java/com/example/cloudcomputingproject/showpost_click_Activity.java
@@ -0,0 +1,14 @@
+package com.example.cloudcomputingproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showpost_click_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showpost_click);
+    }
+}
