ca490544153a70e8748df5835c32e2113cd0f90d
DodoJBNU <dh723@jbnu.ac.kr>
2023-12-04 16:36:57 +0900

    Merge : dohyun_ver3

--- app/src/main/java/com/example/cloudcomputingproject/AddPostActivity.java
+++ app/src/main/java/com/example/cloudcomputingproject/AddPostActivity.java
@@ -1,21 +1,42 @@
 package com.example.cloudcomputingproject;
 
+import static com.google.common.io.Files.getFileExtension;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.cloudcomputingproject.datas.PostDataPut;
 import com.example.cloudcomputingproject.datas.PostDataPutResponse;
 import com.example.cloudcomputingproject.utility.APIInterface;
 import com.example.cloudcomputingproject.utility.RetrofitClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -27,7 +48,13 @@ public class AddPostActivity extends AppCompatActivity {
 
     EditText title_et, contents_et, img_et, price_et, location_et;
     String u_id, title, contents, img, category_label, price, location;
+    //FrameLayout img_fr;
+    ImageView img_iv;
+    ProgressBar progressBar;
 
+    private Uri imageUri;
+
+    private final StorageReference reference = FirebaseStorage.getInstance().getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +69,26 @@ public class AddPostActivity extends AppCompatActivity {
 
         u_id = intent.getStringExtra("u_id"); // 로그인할때 전달해준 u_id를 변수에 저장.
         category_label = intent.getStringExtra("category"); // PostActivity에서 선택한 category 전달.
-    }
+
+        img_iv = findViewById(R.id.img_iv);
+        progressBar = findViewById(R.id.progress_view);
+
+        // 초기에는 프로그래스바 감추기
+        progressBar.setVisibility(View.INVISIBLE);
+
+        img_iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/");
+                activityResult.launch(intent);
+            }
+        });
+
+
+    } // onCreate
+
     /**
      * 툴바 오른쪽에 '저장' 버튼 추가.
      * @param menu The options menu in which you place your items.
@@ -65,12 +111,83 @@ public class AddPostActivity extends AppCompatActivity {
             price = price_et.getText().toString();
             location = location_et.getText().toString();
 
-            img = "temp";
+            if(imageUri != null){
+                uploadToFirebase(imageUri);
+            }
+
+            img = imageUri.toString();
 
             startInsert(new PostDataPut(u_id, title, contents, img, category_label, price, location));
         }
         return super.onOptionsItemSelected(item);
     }
+
+    // 사진 가져오기..
+    ActivityResultLauncher<Intent> activityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>(){
+                public void onActivityResult(ActivityResult result){
+                    if(result.getResultCode() == RESULT_OK && result.getData() != null){
+                        imageUri = result.getData().getData();
+                        Log.d("imageUri는 :", imageUri.toString());
+
+
+                        // ImageView의 레이아웃 파라미터를 가져옵니다.
+                        ViewGroup.LayoutParams params = img_iv.getLayoutParams();
+
+
+                        // 가로와 세로를 MATCH_PARENT로 설정합니다.
+                        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                        params.height = ViewGroup.LayoutParams.MATCH_PARENT;
+
+                        // 변경된 레이아웃 파라미터를 ImageView에 적용합니다.
+                        img_iv.setLayoutParams(params);
+
+                        img_iv.setImageURI(imageUri);
+                    }
+                }
+            }
+    );
+
+    //파이어베이스 이미지 업로드
+    private void uploadToFirebase(Uri uri) {
+
+        StorageReference fileRef = reference.child(System.currentTimeMillis() + "." + getFileExtension(uri.toString()));
+
+        fileRef.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        // 나중에 접근 가능한 uri를 db에 넣자!
+                        Log.d("이미지 파일 업로드 성공 !!", ".");
+
+                        //프로그래스바 숨김
+                        progressBar.setVisibility(View.INVISIBLE);
+
+                        Toast.makeText(AddPostActivity.this, "업로드 성공", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                //프로그래스바 보여주기
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //프로그래스바 숨김
+                progressBar.setVisibility(View.INVISIBLE);
+                Toast.makeText(AddPostActivity.this, "업로드 실패", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     private void startInsert(PostDataPut data) {
         service.PostInsert(data).enqueue(new Callback<PostDataPutResponse>() {
             @Override
@@ -96,4 +213,5 @@ public class AddPostActivity extends AppCompatActivity {
             }
         });
     }
+
 }
--- app/src/main/java/com/example/cloudcomputingproject/utility/RetrofitClient.java
+++ app/src/main/java/com/example/cloudcomputingproject/utility/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://172.30.1.16:3000/";
+    private static final String BASE_URL = "http://61.99.50.147:3000/";
     private static Retrofit retrofit = null;
 
     private RetrofitClient(){
