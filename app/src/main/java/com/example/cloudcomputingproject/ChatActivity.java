8b9edced562881ba1a74815137b9fb9d66eec016
전승혁 <gainthunter@naver.com>
2023-12-06 23:09:45 +0900

    update : 채팅방에서 프로필 누르면 마이페이지 이동

--- app/src/main/java/com/example/cloudcomputingproject/ChatActivity.java
+++ app/src/main/java/com/example/cloudcomputingproject/ChatActivity.java
@@ -11,7 +11,6 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 
