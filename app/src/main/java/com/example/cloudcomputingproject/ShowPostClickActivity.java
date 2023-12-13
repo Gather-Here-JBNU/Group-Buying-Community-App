40873b99152e801e0a82d0cf1bcb70d745e5ea13
전승혁 <gainthunter@naver.com>
2023-12-13 15:28:37 +0900

    update : 카테고리 입력칸 수정

--- app/src/main/java/com/example/cloudcomputingproject/ShowPostClickActivity.java
+++ app/src/main/java/com/example/cloudcomputingproject/ShowPostClickActivity.java
@@ -1,14 +1,18 @@
 package com.example.cloudcomputingproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class ShowPostClickActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showpost_click);
+
+        toolbar = findViewById(R.id.toolbar);
     }
 }
