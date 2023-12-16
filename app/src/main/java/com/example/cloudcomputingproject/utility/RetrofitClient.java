a4d5b612ae7fe4ad87871536611c517438278c54
전승혁 <gainthunter@naver.com>
2023-12-16 13:45:55 +0900

    .

--- app/src/main/java/com/example/cloudcomputingproject/utility/RetrofitClient.java
+++ app/src/main/java/com/example/cloudcomputingproject/utility/RetrofitClient.java
@@ -4,8 +4,9 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    //private static final String BASE_URL = "http://34.201.194.189:3000/";
-    private static final String BASE_URL = "http://10.55.44.153:3000/";
+    //private static final String BASE_URL = "http://34.201.194.189:3000/"; //aws
+    private static final String BASE_URL = "http://10.55.44.153:3000/"; //로컬
+    //private static final String BASE_URL = "http://Group-Buying-Community-App-ALB-1311762057.us-east-1.elb.amazonaws.com:3000/"; //aws 로드 밸런싱
     private static Retrofit retrofit = null;
 
     private RetrofitClient(){
